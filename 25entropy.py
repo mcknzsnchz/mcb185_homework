fb162017421d953560d74196a1f1567b088e9678
username <macsanchez@ucdavis.edu>
2024-04-13 13:28:43 -0700

    new

---
+++ 25entropy.py
@@ -0,0 +1,14 @@
+import math 
+
+# function that returns the Shannon entropy for nucleotide counts a, c, g, t.
+
+def shannon_entropy(dna_seq): 
+	[nt] = ('A' or 'T' or 'C' or 'G')
+	y = p(x)
+	a = - sum(y * mat.log2(y))
+	if s: return print(a)
+
+shannon_entropy('ATCGTCGTAGCTTTGCAAATGTCATGTGTTACATGTGTGGCCTAAAAGTAG')
+
+
+
